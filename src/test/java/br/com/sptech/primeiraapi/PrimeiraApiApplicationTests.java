ac3ce2d11fb1d778356c8b15d38c1dfe472671e6
lucasmsouza24 <lucas.msouza@bandtec.com.br>
2021-12-10 12:48:52 -0300

    first commit

---
+++ src/test/java/br/com/sptech/primeiraapi/PrimeiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sptech.primeiraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
